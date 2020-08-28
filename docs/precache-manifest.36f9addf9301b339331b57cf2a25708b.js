d265d53535cf8c91cd775e4b9963c2e3c61c84ea
Jose Manuel <totezx@hotmail.com>
2020-08-29 00:50:08 +0200

    Docs agregado

---
+++ docs/precache-manifest.36f9addf9301b339331b57cf2a25708b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a0a105567c61e187aca3814dfd34c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1462b9a79d11845cd80",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1462b9a79d11845cd80",
+    "url": "/static/js/main.49854f1b.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
